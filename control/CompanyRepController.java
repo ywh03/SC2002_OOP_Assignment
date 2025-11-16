b65694373485be81fbca8a51600a605a6c54835c
Nandhini Elangovan <nandhini@Nandhinis-MacBook-Air.local>
2025-11-16 23:24:38 +0800

    added register() to CompanyRepController

--- control/CompanyRepController.java
+++ control/CompanyRepController.java
@@ -6,4 +6,9 @@ public class CompanyRepController {
     public void rejectCompanyRep(CompanyRep companyRep){
         companyRep.setApproved(false);
     }
+
+    public void register(String userId, String fullName, String password, String companyName,
+    String department,String position){
+        CompanyRep(userId, fullName, password, companyName, department,position);
+    }
 }
