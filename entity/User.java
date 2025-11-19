d3e2e77e11990f8315c5b0701235ca4bc6a5da1b
clarissa <clacyx29@gmail.com>
2025-11-19 10:33:06 +0800

    userID to userId...

--- entity/User.java
+++ entity/User.java
@@ -3,21 +3,20 @@ package entity;
 import java.io.Serializable;
 
 public abstract class User implements Serializable, Identifiable {
-    private String userID;
+    private String userId;
     private String fullName;
     private String password;
 
-    public User(String userID, String fullName, String password) {
-        this.userID = userID;
+    public User(String userId, String fullName, String password) {
+        this.userId = userId;
         this.fullName = fullName;
         this.password = password;
     }
 
     @Override
-    public String getId() {return userID;}
+    public String getId() {return userId;}
 
-    public String getUserID() {return userID;}
-    public void setUserID(String userID) {this.userID = userID;}
+    public void setUserID(String userId) {this.userId = userId;}
     public String getFullName() {return fullName;}
     public void setFullName(String fullName) {this.fullName = fullName;}
     public String getPassword() {return password;}
