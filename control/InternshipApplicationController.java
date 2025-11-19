ad44a7ac312d8bf2017ea6229ff04e3d62a06060
clarissa <clacyx29@gmail.com>
2025-11-19 10:15:05 +0800

    fixed getters setters and added isELigible and canApply under intappctrl

--- control/InternshipApplicationController.java
+++ control/InternshipApplicationController.java
@@ -6,33 +6,57 @@ import entity.enums.ApplicationStatus;
 import repository.InternshipApplicationRepository;
 import repository.InternshipRepository;
 import entity.Student;
+import entity.enums.InternshipLevel;
+import entity.enums.InternshipStatus;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class InternshipApplicationController{
     private InternshipApplicationRepository internshipApplicationRepository;
     private InternshipRepository internshipRepository;
 
-    public InternshipApplicationController() { // constructor???
+    public InternshipApplicationController() { 
         internshipApplicationRepository = new InternshipApplicationRepository();
         internshipRepository = new InternshipRepository();
     }
 
     public void printApplication() {
-        if (internshipApplicationRepository.getAll().isEmpty()) {
+        if (internshipApplicationRepository.findAll().isEmpty()) {
             System.out.println("No applications were submitted.");
             return;
         }
 
-        for (InternshipApplication intApp : internshipApplicationRepository.getAll()) {
+        for (InternshipApplication intApp : internshipApplicationRepository.findAll()) {
             System.out.println("Internship: " + intApp.getInternship().getInternshipTitle() + ", Student: " + intApp.getStudent().getFullName()
                                 + ", Status: " + intApp.getApplicationStatus() + ", Offer Accepted: " + intApp.getOfferAccepted());
         }
     }
 
     // student accepts the offer
-    public void accept(InternshipApplication intApp) { 
+    public void acceptOffer(InternshipApplication intApp) { 
         if (intApp.getApplicationStatus() == ApplicationStatus.SUCCESSFUL && !intApp.getOfferAccepted()) {
             intApp.setOfferAccepted(true);
             internshipApplicationRepository.save(intApp); //check if update = save
+
+            // after student accepts one internship offer, they will withdraw all their other applications
+            Student student = intApp.getStudent();
+            for (InternshipApplication otherIntApp : student.getAppliedInternships().stream().filter( a -> !a.getId().equals(intApp.getId())).collect((Collectors.toList()))) {
+                internshipApplicationRepository.delete(otherIntApp);
+                // remove from internship list as well
+                Internship otherInternshipApplied = otherIntApp.getInternship();
+                otherInternshipApplied.getInternshipApplications().removeIf(a -> a.getId().equals(otherIntApp.getId()));
+                internshipRepository.save(otherInternshipApplied);
+                // remove from student's list to update it as well
+                student.getAppliedInternships().removeIf(a -> a.getId().equals(otherIntApp.getId()));
+            }
+
+            // check if internshipstatus goes to filled:
+            Internship internship = intApp.getInternship();
+            Long acceptedOffersCount = internship.getInternshipApplications().stream().filter(a -> a.getOfferAccepted()).count();
+            if (acceptedOffersCount >= internship.getNumOfSlots()) {
+                internship.setInternshipStatus(InternshipStatus.FILLED);
+            }
+            internshipRepository.save(internship);
             System.out.println(intApp.getStudent().getFullName() + " has accepted the internship offer.");
         } else {
             System.out.println("Cannot accept offer for this application.");
@@ -57,12 +81,13 @@ public class InternshipApplicationController{
     // student applies for an internship
     public boolean apply(Internship internship, Student student) {
         // if they already applied, do not accept
-        boolean alreadyApplied = internshipApplicationRepository.getAll().stream().anyMatch(a -> a.getInternship() == internship && a.getStudent() == student);
+        boolean alreadyApplied = internshipApplicationRepository.findAll().stream().anyMatch(a -> a.getInternship() == internship && a.getStudent() == student);
             if (alreadyApplied) {
                 System.out.println(student.getFullName() + " has already applied for " + internship.getInternshipTitle());
                 return false;
             }
-
+        
+        // student haven't apply yet -> generate a new id for them
         String newId = internshipApplicationRepository.generateNextId();
         InternshipApplication newIntApp = new InternshipApplication(newId, internship, student);
         internshipApplicationRepository.save(newIntApp);
@@ -74,7 +99,7 @@ public class InternshipApplicationController{
 
     // means application status is successful -> give offer
     public void approveInternshipApplication(InternshipApplication intApp) {
-        if (internshipApplicationRepository.getAll().contains(intApp)) {
+        if (internshipApplicationRepository.findAll().contains(intApp)) {
             intApp.setApplicationStatus(ApplicationStatus.SUCCESSFUL);
             System.out.println("Application approved for " + intApp.getStudent().getFullName());
         } else {
@@ -84,7 +109,7 @@ public class InternshipApplicationController{
 
     // company rejects application -> unsuccessful
     public void rejectInternshipApplication(InternshipApplication intApp) {
-        if (internshipApplicationRepository.getAll().contains(intApp)) {
+        if (internshipApplicationRepository.findAll().contains(intApp)) {
             intApp.setApplicationStatus(ApplicationStatus.UNSUCCESSFUL);
             System.out.println("Application rejected for " + intApp.getStudent().getFullName());
         } else {
@@ -92,8 +117,8 @@ public class InternshipApplicationController{
         }
     }
 
-    public void approveApplicationWithdrawal(InternshipApplication intApp) {
-        if (internshipApplicationRepository.getAll().contains(intApp)) {
+    public void approveAppWithdrawal(InternshipApplication intApp) {
+        if (internshipApplicationRepository.findAll().contains(intApp)) {
             internshipApplicationRepository.delete(intApp);
             intApp.getInternship().getInternshipApplications().remove(intApp);
             System.out.println("Withdrawal approved for " + intApp.getStudent().getFullName());
@@ -102,11 +127,76 @@ public class InternshipApplicationController{
         }
     }
 
-    public void rejectApplicationWithdrawal(InternshipApplication app) {
-        if (internshipApplicationRepository.getAll().contains(app)) {
+    public void rejectAppWithdrawal(InternshipApplication app) {
+        if (internshipApplicationRepository.findAll().contains(app)) {
             System.out.println("Withdrawal rejected for " + app.getStudent().getFullName() + ". Application remains " + app.getApplicationStatus());
         } else {
             System.out.println("Application not found.");
         }
+    }
+
+    public boolean canApply(Student student) {
+        // case 0: student don't exist:
+        if (student == null) {
+            return false;
+        }
+        // case 1: student has already accepted an offer
+        boolean hasAcceptedOffer = student.getAppliedInternships().stream().anyMatch(a -> a.getOfferAccepted());
+        if (hasAcceptedOffer) {
+            return false;
+        }
+        // case 2: student's current applications = 3 already (not incl of unsuccessful ones)
+        long currentIntAppCount = student.getAppliedInternships().stream().filter(a -> a.getApplicationStatus() != ApplicationStatus.UNSUCCESSFUL).count();
+        return currentIntAppCount < 3; // allow up to 3 active applications
+    }
+
+    public boolean isEligible(Student student, Internship internship) {
+        // case 0: student don't exist:
+        if (student == null | internship == null) {
+            return false;
+        }
+        // rej 1: internship status (not sure if this is needed as well but put first lols -- depends on how we're toggling visibility imo)
+        if (internship.getInternshipStatus() != InternshipStatus.APPROVED) {
+            return false;
+        }
+        // rej 2: visbility
+        if (!internship.getVisibility()) {
+            return false;
+        }
+        // rej 3: check application dates (not sure if this one we incl here or not as well)
+        Date today = new Date();
+        Date open = internship.getAppOpenDate();
+        Date close = internship.getAppCloseDate();
+        if (open != null && today.before(open)) return false;
+        if (close != null && today.after(close)) return false;
+
+        // rej 4: check for preferred major (not sure if this one we incl here or not as well)
+        String preferredMajor = internship.getPreferredMajor();
+        if (preferredMajor != null && !preferredMajor.trim().isEmpty()) {
+            if (student.getMajor() == null) return false;
+            if (!preferredMajor.trim().equalsIgnoreCase(student.getMajor().trim())) return false;
+        }
+
+        // rej 5: internship level don't match the year (not sure if this one we incl here or not as well)
+        if (student.getYearOfStudy() <= 2) {
+            if (internship.getLevel() != InternshipLevel.BASIC) return false;
+        }
+
+        // rej 6: internship is filled
+        if (internship.getInternshipStatus() == InternshipStatus.FILLED) return false;
+
+        // rej 7: student applied for this specific internship already
+        if (internship.getInternshipApplications().stream().anyMatch(a -> a.getInternship().getId().equals(internship.getId()))) {
+            return false;
+        }
+
+        // rej 8: student can't apply for anymore internships
+        if (!canApply(student)) return false;
+
+        return true;
+    }
+
+    public String displayInternshipApplications(String InternshipId) {
+
     }
 }
